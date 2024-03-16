478727c67169aa0e73e23ebc9260669761629508
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-03-16 16:34:05 +0200

    add

--- src/main.js/1-timer.js
+++ src/main.js/1-timer.js
@@ -5,10 +5,12 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 let userDate;
-let intervalTime;
-const dataInputTime = document.querySelector('#datetime-picker');
-const startBtn = document.querySelector(`button`);
-const showTime= document.querySelector(`.value`);
+let timeInterval;
+
+
+
+
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -17,16 +19,16 @@ const options = {
   onClose(selectedDates) {
     // console.log(selectedDates[0]);
     userDate = selectedDates[0];
-    intervalTime= userSelectedDate - options.defaultDate;
+    timeInterval = userDate - options.defaultDate;
 
-    if (intervalTime < 1) {
+    if (timeInterval < 1) {
       iziToast.error({
         color: 'red',
         position: 'topRight',
         message: `Please choose a date in the future`,
       });
     } else {
-      startBtn.disabled = false;
+      clickBtn.disabled = false;
       inputTime.disabled = true;
     }
   },
@@ -34,43 +36,52 @@ const options = {
 
 
 function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
-  
-//   console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-//   console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-//   console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-  
-startBtn.disabled = true;
-startBtn.addEventListener('click', event => {
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+
+
+
+
+
+
+const calendar = flatpickr('#datetime-picker', options);
+const inputTime = document.querySelector('#datetime-picker');
+const clickBtn = document.querySelector('button');
+const showTime = document.querySelectorAll('.value');
+console.log(showTime);
+
+clickBtn.disabled = true;
+
+clickBtn.addEventListener('click', event => {
   const repeatTime = setInterval(() => {
-    intervalTime = userDate - new Date();
+    timeInterval = userDate - new Date();
     event.preventDefault();
-    dataInputTime.disabled = true;
+    inputTime.disabled = true;
 
     if (timeInterval < 1) {
-      startBtn.disabled = true;
-      dataInputTime .disabled = false;
+      clickBtn.disabled = true;
+      inputTime.disabled = false;
       clearInterval(repeatTime);
       return;
-    };
-    console.log(showTime);
+    }
+
     const timer = convertMs(timeInterval);
 
     showTime[0].innerText = timer.days.toString().padStart(2, '0');
