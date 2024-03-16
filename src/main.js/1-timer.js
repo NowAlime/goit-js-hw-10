6f1590d59bd42a703fb501a8e4b9c3d13699581c
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-03-16 15:36:03 +0200

    add

--- src/main.js/1-timer.js
+++ src/main.js/1-timer.js
@@ -1,11 +1,14 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 let userSelectedDate;
 let intervalTime;
+const valueTime = flatpickr('#datetime-picker', options);
+const dataInputTime = document.querySelector('#datetime-picker');
+const inputBtn = document.querySelector(`button`);
+const newTime = document.querySelector(`.value`);
 
 
 const startBtn = document.querySelector ('.[data-start]')
@@ -17,24 +20,24 @@ const options = {
     minuteIncrement: 1,
     onClose(selectedDates) {
   userSelectedDate = selectedDates[0];
-  userSelectedDate = intervalTime -options.defaultDate;
+  userSelectedDate = intervalTime - options.defaultDate;
 
       console.log(selectedDates[0]);
 
   
 
-function checkTime (){
-    iziToast.error({
-        color: 'red',
-        position: 'topRight',
-        message: `Please choose a date in the future`,
-      });
-    } else {
-      startBtn.disabled = false;
-      inputTime.disabled = true;
-    }
-  },
-};
+      if (intervalTime< 1) {
+        iziToast.error({
+          color: 'red',
+          position: 'topRight',
+          message: `Please choose a date in the future`,
+        });
+      } else {
+        startBtn.disabled = false;
+        inputTime.disabled = true;
+      }
+    },
+  };
 
 
 function convertMs(ms) {
@@ -56,10 +59,25 @@ function convertMs(ms) {
     return { days, hours, minutes, seconds };
   }
   
-  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+//   console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+//   console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+//   console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
   
 
-  
+inputBtn.addEventListener(`click`, eve) => {
+    const inputTime = setInterval ((){
+        intervalTime = userSelectedDate - new Date();
+        eve.preventDefault();
+        inputTime.disabled = true;
+
+
+        
+    if (intervalTime < 1) {
+        inputBtn.disabled = true;
+        inputTime.disabled = false;
+        clearInterval(repeatTime);
+        return;
+      }
+    }
+}
